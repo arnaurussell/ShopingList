cf1fbf6b0b4a0a4b72dfcd35eaa7865ec35b6f29
arnau <arnaurussell@gmail.com>
2017-10-19 14:26:28 +0200

    lista e la compra parte 1 acabada

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShppingListActivity.java
@@ -1,11 +1,17 @@
 package edu.upc.eseiaat.pma.shoppinglist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -34,10 +40,74 @@ public class ShppingListActivity extends AppCompatActivity {
 
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,itemList);
 
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem();
 
+            }
 
+            private void addItem() {
+                String item_text = edit_item.getText().toString();
+                if (!item_text.isEmpty()){
+                    itemList.add(item_text);
+                    adapter.notifyDataSetChanged();
+                    edit_item.setText("");}
+            }
+        });
 
+        edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                addItem();
+                return true;
+            }
+
+            private void addItem() {
+                String item_text = edit_item.getText().toString();
+                if (!item_text.isEmpty()){
+                    itemList.add(item_text);
+                    adapter.notifyDataSetChanged();
+                    edit_item.setText("");}
+
+            }
+        });
 
         list.setAdapter(adapter);
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> list, View item, int pos, long id) {
+                maybeRemoveItem(pos);
+                return true;
+            }
+        });
+
+
     }
-}
+
+    private void maybeRemoveItem(final int pos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String fmt = getResources().getString(R.string.confirm_mesage);
+        builder.setTitle(R.string.confirm);
+        builder.setMessage(String.format(fmt, itemList.get(pos)));
+        builder.setPositiveButton(R.string.remove, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                itemList.remove(pos);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
+
+
+    }
+
+
+};
+
+
+
