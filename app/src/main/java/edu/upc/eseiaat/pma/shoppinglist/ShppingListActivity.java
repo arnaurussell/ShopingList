1d8ad17fb2bdfdd7ca05f0a9a7d2f8f66aadd59c
arnau <arnaurussell@gmail.com>
2017-10-19 12:02:25 +0200

    layout realizado

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShppingListActivity.java
@@ -2,12 +2,42 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShppingListActivity extends AppCompatActivity {
+
+    private ArrayList<String> itemList;
+    private ArrayAdapter<String> adapter;
+
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shpping_list);
+
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        edit_item = (EditText) findViewById(R.id.edit_item);
+
+        itemList = new ArrayList<>();
+        itemList.add("patatas");
+        itemList.add("gege");
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,itemList);
+
+
+
+
+
+        list.setAdapter(adapter);
     }
 }
